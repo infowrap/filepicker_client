3fc010a2a856c401344dbb3af2d11b24fca0d137
Randy Schmidt <me@r38y.com>
2013-08-05 09:43:42 -0400

    Update the code to use two spaces instead of tabs

--- Rakefile
+++ Rakefile
@@ -1,8 +1,8 @@
 require 'rake/testtask'
 
 Rake::TestTask.new do |t|
-	t.libs << 'test'
-	t.verbose = true
+  t.libs << 'test'
+  t.verbose = true
 end
 
 desc "Run tests"
--- test/test_filepicker_client.rb
+++ test/test_filepicker_client.rb
@@ -3,84 +3,84 @@ require 'tempfile'
 require 'filepicker_client'
 
 class TestFilepickerClient < Test::Unit::TestCase
-	def setup
-		@api_key = ENV['FPAPIKEY']
-		@api_secret = ENV['FPAPISECRET']
-	end
+  def setup
+    @api_key = ENV['FPAPIKEY']
+    @api_secret = ENV['FPAPISECRET']
+  end
 
-	def test_file_uri
-		client = FilepickerClient.new 'fake-key', 'fake-secret'
-		uri = client.file_uri('fake-handle')
-		assert_equal "https://www.filepicker.io/api/file/fake-handle", uri.to_s
-	end
+  def test_file_uri
+    client = FilepickerClient.new 'fake-key', 'fake-secret'
+    uri = client.file_uri('fake-handle')
+    assert_equal "https://www.filepicker.io/api/file/fake-handle", uri.to_s
+  end
 
-	def test_file
-		assert @api_key, "Must set FPAPIKEY for this test"
-		assert @api_secret, "Must set FPAPISECRET for this test"
+  def test_file
+    assert @api_key, "Must set FPAPIKEY for this test"
+    assert @api_secret, "Must set FPAPISECRET for this test"
 
-		client = FilepickerClient.new @api_key, @api_secret
+    client = FilepickerClient.new @api_key, @api_secret
 
-		content = "test file content\n" * 10
+    content = "test file content\n" * 10
 
-		store_file = Tempfile.new('test.txt')
-		store_file.write content
-		store_file.rewind
+    store_file = Tempfile.new('test.txt')
+    store_file.write content
+    store_file.rewind
 
-		write_content = "write content\n" * 10
+    write_content = "write content\n" * 10
 
-		write_file = Tempfile.new('test-write.txt')
-		write_file.write write_content
-		write_file.rewind
+    write_file = Tempfile.new('test-write.txt')
+    write_file.write write_content
+    write_file.rewind
 
-		begin
-			# store
-			file = client.store(store_file, 'test')
+    begin
+      # store
+      file = client.store(store_file, 'test')
 
-			# stat
-			stats = file.stat
-			assert_equal content.length, stats[:size]
-			assert_equal 'text/plain; charset=utf-8', stats[:mime_type]
+      # stat
+      stats = file.stat
+      assert_equal content.length, stats[:size]
+      assert_equal 'text/plain; charset=utf-8', stats[:mime_type]
 
-			# read
-			downloaded_content = file.read
-			assert_equal content, downloaded_content
+      # read
+      downloaded_content = file.read
+      assert_equal content, downloaded_content
 
-			# store_url
-			second_file = client.store_url file.file_read_uri[:uri], 'test'
-			assert_not_equal second_file.handle, file.handle
-			assert_equal second_file.read, content
+      # store_url
+      second_file = client.store_url file.file_read_uri[:uri], 'test'
+      assert_not_equal second_file.handle, file.handle
+      assert_equal second_file.read, content
 
-			# write
-			file.write write_file
+      # write
+      file.write write_file
 
-			downloaded_content = file.read
-			assert_equal write_content, downloaded_content
+      downloaded_content = file.read
+      assert_equal write_content, downloaded_content
 
-			# write_url
-			# - write from second_file to first
-			# - first should have write_content then content after write url
-			file.write_url second_file.file_read_uri[:uri]
-			assert_equal content, file.read
+      # write_url
+      # - write from second_file to first
+      # - first should have write_content then content after write url
+      file.write_url second_file.file_read_uri[:uri]
+      assert_equal content, file.read
 
-			# remove
-			assert file.remove
-			assert second_file.remove
+      # remove
+      assert file.remove
+      assert second_file.remove
 
-			error_fired = false
-			begin
-				file.read
-			rescue Exception => e
-				error_fired = true
-			end
-			assert error_fired, "no error fired when reading a deleted file"
-		rescue Exception => e
-			raise e	# reraise to have error reported
-		ensure
-			store_file.close
-			store_file.unlink
+      error_fired = false
+      begin
+        file.read
+      rescue Exception => e
+        error_fired = true
+      end
+      assert error_fired, "no error fired when reading a deleted file"
+    rescue Exception => e
+      raise e # reraise to have error reported
+    ensure
+      store_file.close
+      store_file.unlink
 
-			write_file.close
-			write_file.unlink
-		end
-	end
+      write_file.close
+      write_file.unlink
+    end
+  end
 end
